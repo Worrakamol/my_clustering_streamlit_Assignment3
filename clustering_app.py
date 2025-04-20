8d258c2bf2ce608f11875abc2cba3fbe35ae6c17
Worrakamol <6531501169@lamduan.mfu.ac.th>
2025-04-20 16:41:35 +0700

    clustering_app.py

--- clustering_app.py
+++ clustering_app.py
@@ -16,7 +16,7 @@ import pandas as pd
 st.set_page_config(page_title="K-Means Clustering App", layout="centered")
 
 # Title
-st.title("🔍 K-Means Clustering App with Iris Dataset")
+st.title("🔍 K-Means Clustering App with Iris Dataset  by Worrakamol Nantipatpanya")
 
 # Load Iris dataset
 iris = load_iris()
